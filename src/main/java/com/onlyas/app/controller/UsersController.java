9f66dc6fd5e1b4c4aca0e5bedf2ab0db9992d108
Danny <1107273369@qq.com>
2019-03-30 17:11:49 +0800

    fix

--- src/main/java/com/onlyas/app/controller/UsersController.java
+++ src/main/java/com/onlyas/app/controller/UsersController.java
@@ -1,9 +1,11 @@
 package com.onlyas.app.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.onlyas.app.dao.UsersMapper;
 import com.onlyas.app.domain.Users;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +18,17 @@ public class UsersController {
     @Autowired
     UsersMapper usersMapper;
 
-    @PostMapping("/list")
-    public List<Users> list(){
+    @GetMapping("/list")
+    public List<Users> list() {
         return usersMapper.selectByExample(null);
+    }
+
+    @GetMapping("/page")
+    public PageInfo<Users> page() {
+        PageHelper.startPage(1, 10);
+        List<Users> list = usersMapper.selectByExample(null);
+        PageInfo<Users> pageInfo = new PageInfo<>(list);
+        return pageInfo;
     }
 
 }
