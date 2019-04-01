66cfe905be0e6375651c968c156616dda1f3e556
Danny <1107273369@qq.com>
2019-04-01 21:53:16 +0800

    fix

--- test.sql
+++ test.sql
@@ -1,10 +1,32 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 5425
+#
+# https://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.24)
+# Database: test
+# Generation Time: 2019-04-01 13:42:41 +0000
+# ************************************************************
 
-# Dump of table News
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+SET NAMES utf8mb4;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table t_news
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `News`;
+DROP TABLE IF EXISTS `t_news`;
 
-CREATE TABLE `News` (
+CREATE TABLE `t_news` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
   `title` varchar(50) DEFAULT NULL COMMENT '标题',
   `intro` varchar(50) DEFAULT NULL COMMENT '简介',
@@ -12,23 +34,23 @@ CREATE TABLE `News` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='新闻表';
 
-LOCK TABLES `News` WRITE;
-/*!40000 ALTER TABLE `News` DISABLE KEYS */;
+LOCK TABLES `t_news` WRITE;
+/*!40000 ALTER TABLE `t_news` DISABLE KEYS */;
 
-INSERT INTO `News` (`id`, `title`, `intro`, `desc`)
+INSERT INTO `t_news` (`id`, `title`, `intro`, `desc`)
 VALUES
 	(1,'测试标题','测试简介','测试内容');
 
-/*!40000 ALTER TABLE `News` ENABLE KEYS */;
+/*!40000 ALTER TABLE `t_news` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
-# Dump of table Users
+# Dump of table t_users
 # ------------------------------------------------------------
 
-DROP TABLE IF EXISTS `Users`;
+DROP TABLE IF EXISTS `t_users`;
 
-CREATE TABLE `Users` (
+CREATE TABLE `t_users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
   `user_name` varchar(50) DEFAULT NULL COMMENT '用户名',
   `user_sex` varchar(50) DEFAULT NULL COMMENT '性别',
@@ -38,14 +60,21 @@ CREATE TABLE `Users` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
-LOCK TABLES `Users` WRITE;
-/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+LOCK TABLES `t_users` WRITE;
+/*!40000 ALTER TABLE `t_users` DISABLE KEYS */;
 
-INSERT INTO `Users` (`id`, `user_name`, `user_sex`, `user_nickname`, `user_birthday`, `user_email`)
+INSERT INTO `t_users` (`id`, `user_name`, `user_sex`, `user_nickname`, `user_birthday`, `user_email`)
 VALUES
 	(1,'danny','man','Danny','1990-01-01 00:00:00','admin@123.com');
 
-/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+/*!40000 ALTER TABLE `t_users` ENABLE KEYS */;
 UNLOCK TABLES;
 
 
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
